a62aabf3170f7cfb284d87200bce477e6c8bcd0e
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-02-01 19:24:05 +0000

    Add files via upload

---
+++ Hypetension Risk Prediction/Scripts/command line.sh
@@ -0,0 +1,21 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2818
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\csgray\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs22 \cf2 \CocoaLigature0 (base) guillermocomesanacimadevila@MacBook-Pro-de-Guille ~ % cd Downloads && pwd\
+/Users/guillermocomesanacimadevila/Downloads\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Downloads % cd /Users/guillermocomesanacimadevila/Desktop\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Desktop % mkdir Hypetension Risk Prediction\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Desktop % cd Hypetension\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Hypetension % mkdir Data\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Hypetension % mkdir Scripts\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Hypetension % cd Data && pwd \
+\
+/Users/guillermocomesanacimadevila/Desktop/Hypetension/Data\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Data % mv /Users/guillermocomesanacimadevila/Downloads/hypertension_dataset.csv /Users/guillermocomesanacimadevila/Desktop/Hypetension/Data\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Data % ls\
+hypertension_dataset.csv\
+(base) guillermocomesanacimadevila@MacBook-Pro-de-Guille Data % gzip hypertension_dataset.csv\
