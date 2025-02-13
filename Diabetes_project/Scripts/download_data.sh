dfb958414c23ef1193dad6e78eacbf03d03317ba
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-02-13 19:21:56 +0000

    Add files via upload

---
+++ Diabetes_project/Scripts/download_data.sh
@@ -0,0 +1,6 @@
+#!bin/bash 
+
+wget curl -L -o ~/Downloads/diabetes-intermediate-dataset.zip\https://www.kaggle.com/api/v1/datasets/download/samira1992/diabetes-intermediate-dataset
+
+file diabetes-intermediate-dataset
+unzip diabetes-intermediate-dataset
